1665a090bce6296e8ad0eca4b425a45a23657fad
TayyarFPM <36998073+TayyarFPM@users.noreply.github.com>
2018-03-02 19:43:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-
+$connectionInfo = array( "Database"=>"TWHDev_DB", "UID"=>"TWHSurvey", "PWD"=>"Getmein@TWHSurvey123$", "Encrypt"=>true,
+                        "TrustServerCertificate"=>false);
+//$serverName="185.12.223.11";
+$serverName="212.36.200.44";
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+if( $conn ) { echo "Connection established."; 
+            }
+else{
+  echo "Connection could not be established.";
+                  //die( print_r( sqlsrv_errors(), true));
+                 }
 ?>
